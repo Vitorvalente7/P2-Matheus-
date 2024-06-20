6b76678865ef2f829374f1d51398a4222e218f84
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-06-20 10:26:21 -0300

    Add files via upload

---
+++ Questão - 3.py
@@ -0,0 +1,12 @@
+n1 = 0
+n2 = 1
+numero_sequencia = int(input("Quantos termos quer que se mostrar? "))
+print('{} -> {}'.format(n1,n2), end='')
+conta = numero_sequencia -2
+while conta !=0:
+    
+    n3 = n1 + n2
+    print(' -> {}'.format(n3),end='')
+    n1 = n2
+    n2 = n3
+    conta = conta-1
