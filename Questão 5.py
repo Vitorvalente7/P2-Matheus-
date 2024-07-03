6150f32b58313e7091e39db4f7257488eca6ad89
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-07-02 21:05:57 -0300

    Add files via upload

---
+++ Questão 5.py
@@ -0,0 +1,4 @@
+lista1 = ["Rossi", "léo Pereira", "Fabricio Bruno", "Varela","Ayrton Lucas"]
+lista2 = ["Pulgar", "De La Cruz", "Arrascaeta", "Cabolinha","Pedro","Gerson"]
+lista3 = list(set(lista1 + lista2))
+print(lista3)
