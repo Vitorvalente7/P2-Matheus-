7cb4fabb3fee45df606b1999718eb8addedd2ad5
Vitorvalente7 <166523368+Vitorvalente7@users.noreply.github.com>
2024-07-03 22:24:37 -0300

    Add files via upload

---
+++ # Questão - 4.py
@@ -0,0 +1,5 @@
+# Questão - 4
+elementos_lista1 = input("Digite os elementos da primeira lista separados por espaço: ")
+elementos_lista2 = input("Digite os elementos da segunda lista separados por espaço: ")
+lista_concatenada = elementos_lista1 + elementos_lista2
+print("Listas concatenadas:", lista_concatenada)
